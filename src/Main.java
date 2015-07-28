3440e3605ebf1181f347861092014b0f720cadfc
Cornelia <cornelia.riem@gym-hip.de>
2015-07-28 16:46:00 +0200

    Projektbasis

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Cornelia on 28.07.2015.
+ */
+public class Main {
+
+    public static void main (String[] args) {
+        Spielfeld spielfeld1 = new Spielfeld ();
+        spielfeld1.ausgeben();
+        System.out.println(spielfeld1.pruefe_Steinsetzen(2));
+    }
+
+}

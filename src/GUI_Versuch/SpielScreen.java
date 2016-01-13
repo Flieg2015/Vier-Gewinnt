b5efebd2d301f910926ab4cf432dfc655db48c12
Thomas <thomas_ludwig@live.de>
2016-01-13 18:11:35 +0100

    SpielScreen mit Bildern

--- src/GUI_Versuch/SpielScreen.java
+++ src/GUI_Versuch/SpielScreen.java
@@ -2,70 +2,76 @@ package GUI_Versuch;
 
 import javax.swing.*;
 import java.awt.*;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 /**
  * Created by tim on 08.01.16.
  */
 public class SpielScreen extends JPanel{
 
-  private JPanel Seite1 = new JPanel(new GridBagLayout());
+    private Bilder meinespielsteine = new Bilder();
+    private JPanel Seite1 = new JPanel(new GridBagLayout());
     private JLabel[][] stein = new JLabel[7][6];
 
     private JButton[] wurfButton = new JButton[7];
 
-    public SpielScreen(){
+    public SpielScreen() {
 
-            GridBagConstraints c = new GridBagConstraints();
-            c.gridx = GridBagConstraints.REMAINDER;
-            c.fill = GridBagConstraints.HORIZONTAL;
-
-
+        GridBagConstraints c = new GridBagConstraints();
+        c.gridx = GridBagConstraints.REMAINDER;
+        c.fill = GridBagConstraints.HORIZONTAL;
 
 
-            //Spielername Label
-            //JLabel Spielfeld = new JLabel("Spielfeld");
-            //c.weightx = 0.0;
-            //c.gridwidth = 0;
-            //c.gridx = 0;
-            //c.gridy = 0;
-            //c.insets = new Insets(0, 0, 0, 0);
-            //Spielscreen.add(Spielfeld, c);
+        //Spielername Label
+        //JLabel Spielfeld = new JLabel("Spielfeld");
+        //c.weightx = 0.0;
+        //c.gridwidth = 0;
+        //c.gridx = 0;
+        //c.gridy = 0;
+        //c.insets = new Insets(0, 0, 0, 0);
+        //Spielscreen.add(Spielfeld, c);
 
 
 
-            //Spielstein
-            int i = 0;
-            int j = 0;
-            for (i = 0; i <= 6; i++){
-                for(j = 0;  j <= 5; j++){
-                    //Methode zum Zeichnen der Steine
+
+        //Spielstein
+        int i = 0;
+        int j = 0;
+        for (i = 0; i <= 6; i++) {
+            for (j = 0; j <= 5; j++) {
+                //Methode zum Zeichnen der Steine
 
 
-                     stein[i][j] = new JLabel("w");
-                    c.weightx = 0.0;
-                    c.gridwidth = 0;
-                    c.gridx = i;
-                    c.gridy = j;
-                    c.insets = new Insets(10, 10, 0, 0);
-                    Seite1.add(stein[i][j], c) ;
-                }
-            }
-
-            // Einwurf-Button
-            i = 0;
-            for(i = 0; i <=6; i++) {
-                wurfButton[i] = new JButton("Einwerfen");
-                Seite1.add(wurfButton[i], c);
-                //c.fill = GridBagConstraints.HORIZONTAL;
+                stein[i][j] = new JLabel(new ImageIcon("C:/Users/Thomas/IdeaProjects/Vier-Gewinnt/pics/rot1.png"));
                 c.weightx = 0.0;
                 c.gridwidth = 1;
                 c.gridx = i;
-                c.gridy = 8;
+                c.gridy = j;
                 c.insets = new Insets(10, 10, 0, 0);
-                Seite1.add(wurfButton[i], c);
+                Seite1.add(stein[i][j], c);
             }
-
         }
+
+        // Einwurf-Button
+        i = 0;
+        for (i = 0; i <= 6; i++) {
+            wurfButton[i] = new JButton("Einwerfen");
+            Seite1.add(wurfButton[i], c);
+            //c.fill = GridBagConstraints.HORIZONTAL;
+            c.weightx = 0.0;
+            c.gridwidth = 1;
+            c.gridx = i;
+            c.gridy = 8;
+            c.insets = new Insets(25, 10, 0, 0);
+            Seite1.add(wurfButton[i], c);
+        }
+
+
+
+
+    }
 
     public JPanel getSeite1() {
         return Seite1;
@@ -83,13 +89,15 @@ public class SpielScreen extends JPanel{
         this.stein = stein;
     }
 
-    public JButton[] getWurfButton() {
-        return wurfButton;
+    public JButton[] getWurfButton() {return wurfButton;
     }
 
     public void setWurfButton(JButton[] wurfButton) {
         this.wurfButton = wurfButton;
     }
+
+
+
 }
 
 

c8f467ef3fdf627ec53f0b12ede360e5ca6f3cdc
tholudwig <thomas_ludwig@live.de>
2016-03-27 10:19:43 +0200

    Update SpielScreen.java

--- src/GUI_Versuch/SpielScreen.java
+++ src/GUI_Versuch/SpielScreen.java
@@ -6,6 +6,7 @@ import db.spieler.SpielerDAOFactory;
 import db.spieler.SpielerDAOJDBCImpl;
 import logik.Spiel;
 
+
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
@@ -14,7 +15,7 @@ import java.util.ArrayList;
 /**
  * Created by tim on 08.01.16.
  */
-public class SpielScreen extends JPanel {
+public class SpielScreen extends JPanel{
 
     private Bilder meinespielsteine = new Bilder();
     private JPanel Seite1 = new JPanel(new GridBagLayout());
@@ -30,26 +31,29 @@ public class SpielScreen extends JPanel {
     private JTextArea s2;
 
 
+
     private JLabel tlabel = new JLabel();
-    private ImageIcon Weiss = new ImageIcon("src/pics/weiss1.png");
-    private ImageIcon Rot = new ImageIcon("src/pics/rot1.png");
-    private ImageIcon Gelb = new ImageIcon("src/pics/gelb1.png");
-    private ImageIcon Rot_Sieg = new ImageIcon("src/pics/rot1_gewinnt1.png");        // ImageIcon fuer roten Sieg
-    private ImageIcon Gelb_Sieg = new ImageIcon("src/pics/gelb1_gewinnt1.png");      // ImageIcon fuer gelben Sieg
-    private JLabel dummy = new JLabel();
+    private ImageIcon Weiss= new ImageIcon("src/pics/weiss1.png");
+    private ImageIcon Rot= new ImageIcon("src/pics/rot1.png");
+    private ImageIcon Gelb= new ImageIcon("src/pics/gelb1.png");
+    private ImageIcon Rot_Sieg= new ImageIcon("src/pics/rot1_gewinnt1.png");        // ImageIcon fuer roten Sieg
+    private ImageIcon Gelb_Sieg= new ImageIcon("src/pics/gelb1_gewinnt1.png");      // ImageIcon fuer gelben Sieg
+    private JLabel dummy1 = new JLabel();
+    private JLabel dummy2 = new JLabel();
 
     private JButton[] wurfButton = new JButton[7];
-    private JButton replayButton = new JButton("Nochmal Spielen");
+    private JButton nochmalButton = new JButton("Nochmal Spielen");
     private JButton wechselnButton = new JButton("Spielmodus wechseln");
 
     private JTextArea bl = new JTextArea(5, 20);
 
+
     public SpielScreen() {
-        s1 = new JTextArea(5, 20);
+        s1 = new JTextArea(5,20);
         s1.setLineWrap(true); //Zeilenumbruch wird eingeschaltet
         s1.setWrapStyleWord(true); //Zeilenumbrüche erfolgen nur nach ganzen Wörtern
 
-        s2 = new JTextArea(5, 20);
+        s2 = new JTextArea(5,20);
         s2.setLineWrap(true);  //Zeilenumbruch wird eingeschaltet
         s2.setWrapStyleWord(true); //Zeilenumbrüche erfolgen nur nach ganzen Wörtern
 
@@ -84,7 +88,7 @@ public class SpielScreen extends JPanel {
         cs.insets = new Insets(10, 10, 10, 10);
         Seite1.add(s2, cs);
 
-        // Sestenliste Anordnung
+    // Bestenliste Anordnung
 
         cs.weightx = 0.0;
         cs.gridwidth = 1;
@@ -110,11 +114,12 @@ public class SpielScreen extends JPanel {
                 //Methode zum Zeichnen der Steine
 
 
+
                 stein[i][j] = new JLabel(Weiss);
                 cs.weightx = 0.0;
                 cs.gridwidth = 1;
-                cs.gridx = i + 1;
-                cs.gridy = j + 1;
+                cs.gridx = i+1;
+                cs.gridy = j+1;
                 cs.insets = new Insets(10, 10, 10, 10);
                 Seite1.add(stein[i][j], cs);
             }
@@ -127,22 +132,30 @@ public class SpielScreen extends JPanel {
             //c.fill = GridBagConstraints.HORIZONTAL;
             cs.weightx = 0.0;
             cs.gridwidth = 1;
-            cs.gridx = i + 1;
+            cs.gridx = i+1;
             cs.gridy = 8;
             cs.insets = new Insets(25, 10, 0, 0);
             Seite1.add(wurfButton[i], cs);
         }
 
-        dummy = new JLabel("");
+        dummy1 = new JLabel("");
         cs.weightx = 0.0;
         cs.weighty = 0.0;
         cs.gridx = 0;
-        cs.gridy = 9;
+        cs.gridy = 10;
         cs.insets = new Insets(10, 10, 10, 10);
-        Seite1.add(dummy, cs);
+        Seite1.add(dummy1, cs);
+
+        dummy2 = new JLabel("");
+        cs.weightx = 0.0;
+        cs.weighty = 0.0;
+        cs.gridx = 0;
+        cs.gridy = 11;
+        cs.insets = new Insets(10, 10, 10, 10);
+        Seite1.add(dummy2, cs);
 
         // Replay-Button
-        replayButton = new JButton("Neues Spiel");
+        nochmalButton = new JButton("Neues Spiel");
         cs.fill = GridBagConstraints.BOTH;
         cs.weightx = 0.0;
         cs.weighty = 0.0;
@@ -151,8 +164,8 @@ public class SpielScreen extends JPanel {
         cs.gridy = 9;
         cs.insets = new Insets(10, 10, 10, 10);
 
-        Seite1.add(replayButton, cs);
-        replayButton.setVisible(false);
+        Seite1.add(nochmalButton, cs);
+        nochmalButton.setVisible(false);
 
         // Modus Wechseln Button
         wechselnButton = new JButton("Neuer Modus");
@@ -167,7 +180,11 @@ public class SpielScreen extends JPanel {
         wechselnButton.setVisible(false);
 
 
+
         //Seite1.add(Feld, java.awt.BorderLayout.CENTER);
+
+
+
 
 
     }
@@ -188,6 +205,7 @@ public class SpielScreen extends JPanel {
             wurfButton[i].setText("Einwerfen");
         }
     }
+
 
     public void sperreButton(int i) {       // sperren eines Einwurf-Button, wenn Spalte voll
         wurfButton[i].setEnabled(false);
@@ -211,22 +229,18 @@ public class SpielScreen extends JPanel {
     }
 
     public void aktiviereSpielwiederholungsButton() {   // macht den Spielwiederholungsbutton sichtbar
-        replayButton.setVisible(true);
+        nochmalButton.setVisible(true);
         wechselnButton.setVisible(true);
     }
 
     public void deaktiviereSpielwiederholungsButton() {   // macht den Spielwiederholungsbutton sichtbar
-        replayButton.setVisible(false);
+        nochmalButton.setVisible(false);
         wechselnButton.setVisible(false);
     }
 
-    public JButton getReplayButton() {
-        return replayButton;
-    }
 
-    public JButton getWechselnButton() {
-        return wechselnButton;
-    }
+    public JButton getNochmalButton(){return nochmalButton;}
+    public JButton getWechselnButton(){return wechselnButton;}
 
 
     public JPanel getSeite1() {
@@ -243,38 +257,39 @@ public class SpielScreen extends JPanel {
 
     public void setStein(int i, int j, int farbe) {
 
-        if (farbe == 1) stein[i][j].setIcon(Rot);
-        if (farbe == 0) stein[i][j].setIcon(Weiss);
-        if (farbe == 2) stein[i][j].setIcon(Gelb);
+        if(farbe==1)  stein[i][j].setIcon(Rot);
+        if(farbe==0)  stein[i][j].setIcon(Weiss);
+        if(farbe==2)  stein[i][j].setIcon(Gelb);
 
     }
 
     public void setSteinSieg(int i, int j, int farbe) {     // Methode zum markieren der siegreichen Steinkombination(en)
 
-        if (farbe == 1) stein[i][j].setIcon(Rot_Sieg);
-        if (farbe == 2) stein[i][j].setIcon(Gelb_Sieg);
+        if(farbe==1)  stein[i][j].setIcon(Rot_Sieg);
+        if(farbe==2)  stein[i][j].setIcon(Gelb_Sieg);
 
     }
 
 
-    public JButton getWurfButton(int i) {
-        return wurfButton[i];
+    public JButton getWurfButton(int  i) {return wurfButton[i];
     }
+
+
 
 
     public void setWurfButton(JButton[] wurfButton) {
         this.wurfButton = wurfButton;
     }
 
-    public void setTextInfo(Spieler spieler1, Spieler spieler2) {
+    public void setTextInfo(Spieler spieler1, Spieler spieler2){
 
         s1.setText("Spieler 1\n " +
-                spieler1.getName() + "\n" +
+                spieler1.getName()+"\n" +
                 "Dein Score \n" +
                 spieler1.getHighscore());
 
         s2.setText("Spieler 2\n " +
-                spieler2.getName() + "\n" +
+                spieler2.getName()+"\n" +
                 "Dein Score \n" +
                 spieler2.getHighscore());
 
@@ -291,7 +306,7 @@ public class SpielScreen extends JPanel {
 
     public void aktualsiereBestenliste(Spieler Gewinner, Spieler Verlierer) {
         SpielerDAO dummy = SpielerDAOFactory.createSpielerDAO();
-String bestenText="Bestenliste\n";
+        String bestenText="Bestenliste\n";
 
 
         Spieler spieler;
@@ -307,12 +322,12 @@ String bestenText="Bestenliste\n";
             liste.remove(0);
 
             if (spieler.getName().equals(Gewinner.getName()) || Gewinner.getName().equals("Computer")  ) {
-                 gewinnerdabei=true;
+                gewinnerdabei=true;
 
             }
             if (spieler.getName().equals(Gewinner.getName()) ||  Gewinner.getName().equals("Computer")) {
 
-               verlieredabei=true;
+                verlieredabei=true;
 
             }
             String einfuegen=spieler.getName()+" " + spieler.getHighscore()+"\n";
@@ -335,13 +350,13 @@ String bestenText="Bestenliste\n";
         }
 
 
-bl.setText(bestenText);
+        bl.setText(bestenText);
 
     }
 
     public void bestenlistenangeben(Spieler Gewinner, Spieler Verlierer) {
-       aktualsiereBestenliste(Gewinner, Verlierer);
-bl.setVisible(true);
+        aktualsiereBestenliste(Gewinner, Verlierer);
+        bl.setVisible(true);
 
     }
 

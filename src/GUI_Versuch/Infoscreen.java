9d6dfd03734a6bdd7ed211234237f65894a2c606
tholudwig <thomas_ludwig@live.de>
2016-03-27 10:19:06 +0200

    Update Infoscreen.java

--- src/GUI_Versuch/Infoscreen.java
+++ src/GUI_Versuch/Infoscreen.java
@@ -2,46 +2,92 @@ package GUI_Versuch;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 
 /**
  * Created by Thomas on 22.02.2016.
  */
-public class Infoscreen extends JPanel {
+public class InfoScreen extends JPanel implements ActionListener {
 
-    JPanel infoSeite = new JPanel(new GridBagLayout());
-    JLabel titel = new JLabel("Anleitung zu 4gewinnt");
+
     JFrame infoframe = new JFrame();
+    JPanel infoSeite = new JPanel(new GridBagLayout());
+    JLabel titel = new JLabel("Informationen");
+    JLabel regeln = new JLabel();
 
 
-    public Infoscreen() {
-        infoframe.setSize(720, 480);
-        infoframe.setTitle("Spieleanleitung");
+    JButton beendenButton = new JButton("Information schliessen");
+    //private ImageIcon  regeln = new ImageIcon("src/pics/regeln.png");
 
 
-        GridBagConstraints ca = new GridBagConstraints();
-        ca.gridx = GridBagConstraints.REMAINDER;
-        ca.fill = GridBagConstraints.HORIZONTAL;
+    public InfoScreen() {
+        infoframe.setSize(750, 550);
+        infoframe.setTitle("Information");
+        infoframe.add(infoSeite);
+
+
+        GridBagConstraints c = new GridBagConstraints();
+        c.gridx = GridBagConstraints.REMAINDER;
+        //c.fill = GridBagConstraints.HORIZONTAL;
+        c.fill = GridBagConstraints.NONE;
 
         //Titel Label
         titel.setFont(new Font("Arial", Font.BOLD, 25));
-        ca.weightx = 0.0;
-        ca.gridwidth = 0;
-        ca.gridx = 0;
-        ca.gridy = 0;
-        ca.insets = new Insets(0, 35, 25, 25);
-        infoSeite.add(titel, ca);
+        c.weightx = 0.0;
+        c.gridwidth = 0;
+        c.gridx = 0;
+        c.gridy = 0;
+        c.insets = new Insets(25, 25, 25, 25);
+        infoSeite.add(titel, c);
+
+
+
+        /**regeln = new JLabel(new ImageIcon("src/pics/info.png"));
+        c.weightx = 0;
+        c.gridwidth = 0;
+        c.gridx = 0;
+        c.gridy = 2;
+        c.insets = new Insets(10, 10, 10, 10);
+        infoSeite.add(regeln);
+         */
+
+        // Hilfe Schliessen Button
+        c.weightx = 0.0;
+        c.weighty = 0.0;
+        c.gridwidth = 2;
+        c.gridx = 0;
+        c.gridy = 3;
+        c.insets = new Insets(10, 10, 0, 0);
+        infoSeite.add(beendenButton, c);
+        beendenButton.addActionListener(this);
+
     }
+
+    public void actionPerformed(ActionEvent evt)
+    {	infoframe.dispose();  //Schließen des AdminFrames
+    }
+
+
 
     public JPanel getInfoSeite() {
         return infoSeite;
     }
 
-
     public void setInfoSeite(JPanel infoSeite) {
         this.infoSeite = infoSeite;
     }
 
-        public void aktiviereInfoScreen(){
+    public void aktiviereHilfeScreen(){
         infoframe.setVisible(true);
+    }
+
+    public JButton getBeendenButton() {
+        return beendenButton;
+    }
+
+    public void setBeendenButton(JButton beendenButton) {
+        this.beendenButton = beendenButton;
     }
 }
